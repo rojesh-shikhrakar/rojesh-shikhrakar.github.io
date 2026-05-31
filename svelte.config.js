8100c0a8228aae39e7a0eb1c95f9dbf953a3543c
rojesh <3447008+rojesh-shikhrakar@users.noreply.github.com>
2026-06-01 00:12:41 +0545

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,15 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		prerender: {
+			handleHttpError: 'warn'
 		}
 	},
 	preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
